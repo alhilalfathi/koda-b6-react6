0889a3bc7eb520b21a7a04e8c2758f88784344a0
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-27 16:55:32 +0700

    feat: add link to admin and home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { TfiWrite } from "react-icons/tfi";
 import { BsBell } from "react-icons/bs";
@@ -10,13 +11,17 @@ export const Header = () => {
             <nav className="flex justify-between h-20 w-screen border border-b-2">
                 <div className="flex items-center gap-5 px-10">
                     <GiHamburgerMenu className="w-8 h-8" />
-                    <h1 className="text-3xl font-bold">Medium</h1>
+                    <Link to="/">
+                        <h1 className="text-3xl font-bold">Medium</h1>
+                    </Link>
                     <input type="text" placeholder="Search" className="border border-gray-200 rounded-2xl p-2 "/>
                 </div>
                 <div className="flex items-center gap-5 px-10">
                     <div className="flex items-center gap-3">
                         <TfiWrite className="w-6 h-6" />
-                        <span className="text-xl">Write</span>
+                        <Link to="/admin">
+                            <span className="text-xl">Write</span>
+                        </Link>
                     </div>
                     <BsBell className="w-6 h-6" />
                     <CgProfile className="w-8 h-8" />

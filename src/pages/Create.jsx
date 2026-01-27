924020f55016a51acf5e77a8e4f241bb148491c4
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-27 21:59:19 +0700

    feat: add input form and script to save to local storage

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,12 +1,49 @@
-
+import { useForm } from "react-hook-form"
+import jsonData from "../../public/article.json"
 
 export const Create = () => {
+    const {handleSubmit, register} = useForm()
+
+    function submitForm(value){
+        const oldArticle = JSON.parse(localStorage.getItem("articleData")) || []
+        const totalArticle = oldArticle.length + jsonData.length
+        const newArticle = {
+        id: totalArticle + 1,              
+        title: value.title,
+        slug: value.slug,
+        body: value.body,
+        name: "Hilal",               
+        username: "@hils",
+        image: "https://placehold.co/350x200"
+        }
+
+        const updatedArticle = [...oldArticle, newArticle]
+        localStorage.setItem("articleData",JSON.stringify(updatedArticle))
+    }
+
   return (
     <div>
-        <div className="flex flex-col gap-3">
-            <label htmlFor="title" className="text-2xl font-bold">Title:</label>
-            <input type="text" id="title" placeholder="Input Title" className="bg-white p-5" />
+        <div>
+            <h1 className="text-4xl font-bold px-5">Create Article</h1>
+            <p className="text-gray-600 px-6 mb-8">Create your own article</p>
         </div>
+        <form onSubmit={handleSubmit(submitForm)}>
+            <div className="flex flex-col gap-3 px-10 py-5">
+                <label htmlFor="title" className="text-2xl font-bold">Title:</label>
+                <input {...register("title")} type="text" id="title" placeholder="Input Title" className="bg-white p-5" />
+            </div>
+            <div className="flex flex-col gap-3 px-10 py-5">
+                <label htmlFor="slug" className="text-2xl font-bold">Slug:</label>
+                <input {...register("slug")} type="text" id="slug" placeholder="Input Slug" className="bg-white p-5" />
+            </div>
+            <div className="flex flex-col gap-3 px-10 py-5">
+                <label htmlFor="body" className="text-2xl font-bold">Article body:</label>
+                <input {...register("body")} type="text" id="body" placeholder="Input article body" className="bg-white px-5 h-40" />
+            </div>
+            <div>
+                <button className="bg-black text-white p-3 rounded-2xl ml-10 hover:bg-gray-900">Submit</button>
+            </div>
+        </form>
     </div>
   )
 }
